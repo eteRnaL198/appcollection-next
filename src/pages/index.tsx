2ddec7e1235740365865e9b83b61559abfe204a9
eternal198 <ikuya313198@gmail.com>
2021-06-13 11:08:26 +0900

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,6 @@ const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
     </p>
   </Layout>
 )
